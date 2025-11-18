ae5d6525c0e291d8715398196352d2bc49750a94
Marina Feller <marifeller523@gmail.com>
2025-11-18 16:18:04 -0700

    Updated FAB design to be consistent with the Figma design, with working buttons

--- aurora/src/components/FloatingActionButton.js
+++ aurora/src/components/FloatingActionButton.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import './FloatingActionButton.css';
+import React from "react";
+import "./FloatingActionButton.css";
 
-const FloatingActionButton = ({ onClick, children }) => {
+// Import SVGs as React Components
+import { ReactComponent as ArtIcon } from "../assets/fab-art.svg";
+import { ReactComponent as AboutIcon } from "../assets/fab-about.svg";
+import { ReactComponent as CadIcon } from "../assets/fab-cad.svg";
+
+const FloatingActionButton = () => {
+  const handleHomeClick = () => {
+    alert("Home clicked!");
+  };
+
+  const handleArtClick = () => {
+    alert("Art clicked!");
+  };
+
+  const handleAboutClick = () => {
+    alert("About clicked!");
+  };
+
+  const handleCadClick = () => {
+    alert("CAD clicked!");
+  };
+
   return (
-    <button className="fab" onClick={onClick}>
-      {children}
-    </button>
+    <div className="fab-container">
+      {/* Central Circular Button */}
+      <button className="fab-btn fab-center" onClick={handleHomeClick}></button>
+
+      {/* Surrounding Sliver Buttons */}
+      <ArtIcon
+        className="fab-btn fab-sliver fab-art"
+        onClick={handleArtClick}
+      />
+
+      <AboutIcon
+        className="fab-btn fab-sliver fab-about"
+        onClick={handleAboutClick}
+      />
+
+      <CadIcon
+        className="fab-btn fab-sliver fab-cad"
+        onClick={handleCadClick}
+      />
+    </div>
   );
 };
 
