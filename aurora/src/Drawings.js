c15dd6872b051beeed6e499dd714e227094aa2e6
Nevtrality <james_towle@outlook.com>
2025-11-21 21:11:03 -0700

    added scrolling on the models and drawings pages and added comments

--- aurora/src/Drawings.js
+++ aurora/src/Drawings.js
@@ -5,22 +5,21 @@ import "./fonts/BOMBARD_.ttf";
 import FloatingActionButton from "./components/FloatingActionButton";
 
 export default function Drawings() {
-    /* Brief rundown of the structure here since comments cant be in a return:
-    Content: main holder for the page, keeps it nice and tidy
-    topBar: creates a cool design for the top of the page
-    scrollable(?): if decide to keep topBar at top, this will hold scrollable material
-    textPortion: holds the top portion of text before the actually cool content*/
     return (
         <>
+        {/* Wrapper class */}
         <div className="content">
+            {/* Creates a cool design for the top of the page */}
             <div className="top">
                 <svg className="topBar" viewBox='0 0 100 100' preserveAspectRatio='none'>
+                    {/* Left bar */}
                     <path d="M   0   0
                     L 0  100
                     L 70 100
                     L 67 0
                     Z"
                     fill="#0BE3BF" />
+                    {/* Right bar */}
                     <path d="M   70   0
                     L 73  100
                     L 100 100
@@ -29,13 +28,18 @@ export default function Drawings() {
                     fill="#0BE3BF" />
                 </svg>
             </div>
+            {/* Header and sub header that appear before items */}
             <div className="text">
+                {/* Header */}
                 <text className="headingText">Drawing Page Description</text>
+                {/* Sub-Header (seperate lines cause it was being a nuisance) */}
                 <text className='subHeadingText'>Can you remind me who I am?</text>
                 <text className='subHeadingText'>Get the president on the phone now</text>
                 <text className='subHeadingText'>I fronted him a brick, I need my money</text>
             </div>
+            {/* Drawings wrapper class; holds all the drawings and links to their pages */}
             <div className="items">
+                {/* Items are currently just colored boxes, will make them fancy and functional once we've got images */}
                 <div className="itemBox1"></div>
                 <div className="itemBox2"></div>
                 <div className="itemBox1"></div>
--- aurora/src/Models.js
+++ aurora/src/Models.js
@@ -7,15 +7,19 @@ import FloatingActionButton from "./components/FloatingActionButton";
 export default function Models(){
     return(
         <>
+        {/* Wrapper class */}
         <div className="content">
+            {/* Creates a cool design for the top of the page */}
             <div className="top">
                 <svg className="topBar" viewBox='0 0 100 100' preserveAspectRatio='none'>
+                    {/* Left bar */}
                     <path d="M   0   0
                     L 0  100
                     L 70 100
                     L 67 0
                     Z"
                     fill="#0BE3BF" />
+                    {/* Right bar */}
                     <path d="M   70   0
                     L 73  100
                     L 100 100
@@ -24,13 +28,18 @@ export default function Models(){
                     fill="#0BE3BF" />
                 </svg>
             </div>
+            {/* Header and sub header that appear before items */}
             <div className="text">
+                {/* Header */}
                 <text className="headingText">CAD Page Description</text>
+                {/* Sub-Header (seperate lines cause it was being a nuisance) */}
                 <text className='subHeadingText'>We straight gassin', cuttin' straight to the bricks, haha</text>
                 <text className='subHeadingText'>This shit ain't nothin' to me, man</text>
                 <text className='subHeadingText'>We smokin' runtz</text>
             </div>
+            {/* Models wrapper class; holds all the model images and links to their pages */}
             <div className="items">
+                {/* Items are currently just colored boxes, will make them fancy and functional once we've got images */}
                 <div className="moditemBox1"></div>
                 <div className="moditemBox2"></div>
                 <div className="moditemBox1"></div>
