ae4401786e0ada4e34424049714894e7312b7b71
Nevtrality <james_towle@outlook.com>
2025-12-05 14:00:29 -0700

    put in images, currently hard coded and limited but want to fix later

--- aurora/src/Drawings.js
+++ aurora/src/Drawings.js
@@ -1,14 +1,119 @@
-import './Drawings.css'
-import { useState } from "react"
+import './Drawings.css';
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./fonts/BOMBARD_.ttf";
 import FloatingActionButton from "./components/FloatingActionButton";
-import image1 from "./assets/tempDraw1.jpg";
-import image2 from "./assets/tempDraw2.jpg";
+import images from "./imageLoader.js";
+import image1 from "./assets/golden age/IMG_0212.PNG";
+import image2 from "./assets/golden age/IMG_0213.PNG";
+import image3 from "./assets/golden age/IMG_0214.PNG";
+import image4 from "./assets/golden age/IMG_0245.JPG";
+import image5 from "./assets/golden age/IMG_0250.JPG";
+import image6 from "./assets/golden age/IMG_0251.JPG";
+import image7 from "./assets/golden age/IMG_0252.JPG";
+import image8 from "./assets/golden age/IMG_0253.JPG";
+import image9 from "./assets/golden age/IMG_0254.JPG";
+import image10 from "./assets/golden age/IMG_0255.JPG";
+import image11 from "./assets/golden age/IMG_0256.JPG";
+import image12 from "./assets/golden age/IMG_0257.JPG";
+import image13 from "./assets/golden age/IMG_0258.JPG";
+import image14 from "./assets/golden age/IMG_0259.JPG";
+import image15 from "./assets/golden age/IMG_0260.JPG";
+import image16 from "./assets/golden age/IMG_0261.JPG";
+import image17 from "./assets/golden age/IMG_0262.JPG";
+import image18 from "./assets/golden age/IMG_0263.GIF";
+import image19 from "./assets/golden age/IMG_0264.JPG";
+import image20 from "./assets/golden age/IMG_0265.JPG";
+import image21 from "./assets/golden age/IMG_0266.JPG";
+import image22 from "./assets/golden age/IMG_0267.GIF";
+import image23 from "./assets/golden age/IMG_0268.JPG";
+import image24 from "./assets/golden age/IMG_0269.JPG";
+import image25 from "./assets/golden age/IMG_0270.JPG";
+import image26 from "./assets/golden age/IMG_0271.JPG";
+import image27 from "./assets/golden age/IMG_0272.JPG";
+import image28 from "./assets/golden age/IMG_0273.JPG";
+import image29 from "./assets/golden age/IMG_0274.JPG";
+import image30 from "./assets/golden age/IMG_0275.GIF";
+import image31 from "./assets/golden age/IMG_0276.JPG";
+import image32 from "./assets/golden age/IMG_0277.JPG";
+import image33 from "./assets/golden age/IMG_0278.JPG";
+import image34 from "./assets/golden age/IMG_0279.JPG";
+import image35 from "./assets/golden age/IMG_0280.JPG";
+import image36 from "./assets/golden age/IMG_0281.JPG";
+import image37 from "./assets/golden age/IMG_0282.JPG";
+import image38 from "./assets/golden age/IMG_0283.JPG";
+import image39 from "./assets/golden age/IMG_0284.JPG";
+import image40 from "./assets/golden age/IMG_0285.JPG";
+import image41 from "./assets/golden age/IMG_0286.JPG";
+import image42 from "./assets/golden age/IMG_0287.JPG";
+import image43 from "./assets/golden age/IMG_0288.JPG";
+import image44 from "./assets/golden age/IMG_0289.JPG";
+import image45 from "./assets/golden age/IMG_0290.JPG";
+import image46 from "./assets/golden age/IMG_0291.JPG";
+import image47 from "./assets/golden age/IMG_0292.JPG";
+import image48 from "./assets/golden age/IMG_0296.PNG";
+import image49 from "./assets/golden age/IMG_0297.PNG";
+import image50 from "./assets/golden age/IMG_0298.JPG";
+import image51 from "./assets/golden age/IMG_0299.JPG";
+import image52 from "./assets/golden age/IMG_0300.JPG";
+import image53 from "./assets/golden age/IMG_0301.JPG";
+import image54 from "./assets/golden age/IMG_0302.JPG";
+import image55 from "./assets/golden age/IMG_0303.JPG";
+import image56 from "./assets/golden age/IMG_0304.JPG";
+import image57 from "./assets/golden age/IMG_0305.JPG";
+import image58 from "./assets/golden age/IMG_0306.JPG";
 
 export default function Drawings() {
     const navigate = useNavigate();
+
+    // drawings stuff
+    const column1 = document.createElement("div");
+    column1.className = "itemColumn";
+    const column2 = document.createElement("div");
+    column2.className = "itemColumnMid";
+    const column3 = document.createElement("div");
+    column3.className = "itemColumnMid";
+
+    let imgList = [];
+
+    {images.map((image, index) =>(
+        imgList[index] = image
+    ))}
+
+    let num = 0;
+    for (const img of imgList){
+        const itemBox = document.createElement("div");
+        itemBox.className="itemBox1";
+
+        const image = document.createElement("img");
+        image.src = img;
+        image.alt = "drawing";
+        itemBox.appendChild(image);
+
+        const desc = document.createElement("div");
+        desc.className = "itemDescription";
+        desc.textContent = "description";
+        itemBox.appendChild(desc);
+
+        switch(num){
+            case 0:
+                column1.appendChild(itemBox);
+                num = 1;
+                break;
+            case 1:
+                column2.appendChild(itemBox);
+                num = 2;
+                break;
+            case 2:
+                column3.appendChild(itemBox);
+                num = 0;
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <>
         {/* Wrapper class */}
@@ -45,32 +150,32 @@ export default function Drawings() {
             <div className="items">
                 {/* Items are currently just colored boxes, will make them fancy and functional once we've got images */}
                 <div className="itemColumn">
-                    <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 1",image:image1}})}> {/*Holy moley that actually worked, item pages are dynamic now*/}
-                        <img src={image1}/>
-                        <div className="itemDescription">Temporary drawing I had saved, will be replaced later once we have others</div>
-                    </div>
-                    <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 2",image:image1}})}>
-                        <img src={image1}/>
-                        <div className="itemDescription">Description</div>
-                    </div>
-                </div>
-                <div className="itemColumnMid" >
-                    <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 3",image:image2}})}>
-                        <img src={image2}/>
-                        <div className="itemDescription">This one serves to test different image sizes. I wanted to keep the cool indenting, but couldn't get it to work dynamically.</div>
-                    </div>
-                    <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 4",image:image2}})}>
-                        <img src={image2}/>
-                        <div className="itemDescription">Description</div>
-                    </div>
-                </div>
-                <div className="itemColumn">
                     <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 5",image:image1}})}>
                         <img src={image1}/>
                         <div className="itemDescription">Description</div>
                     </div>
-                    <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 6",image:image1}})}>
-                        <img src={image1}/>
+                    <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 6",image:image2}})}>
+                        <img src={image2}/>
+                        <div className="itemDescription">Description</div>
+                    </div>
+                </div>
+                <div className="itemColumnMid" >
+                    <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 3",image:image3}})}>
+                        <img src={image3}/>
+                        <div className="itemDescription">This one serves to test different image sizes. I wanted to keep the cool indenting, but couldn't get it to work dynamically.</div>
+                    </div>
+                    <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 4",image:image4}})}>
+                        <img src={image4}/>
+                        <div className="itemDescription">Description</div>
+                    </div>
+                </div>
+                <div className="itemColumn">
+                    <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 5",image:image5}})}>
+                        <img src={image5}/>
+                        <div className="itemDescription">Description</div>
+                    </div>
+                    <div className="itemBox1" onClick={()=>navigate("/item",{state:{item:"Drawing 6",image:image6}})}>
+                        <img src={image6}/>
                         <div className="itemDescription">Description</div>
                     </div>
                 </div>
