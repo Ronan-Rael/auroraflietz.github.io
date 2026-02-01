77e0a7810556e52b19d39aae503cb0f793a9e525
Ronan-Rael <rtrael@mines.edu>
2026-02-01 12:50:27 -0700

    renamed root folder

--- aurora/src/imageLoader.js
+++
@@ -1,9 +0,0 @@
-function importAll(r) {
-    return r.keys().map(r);
-  }
-
-  const images = importAll(
-    require.context("./assets/golden age", false, /\.(PNG|JPG)$/)
-  );
-
-  export default images;
